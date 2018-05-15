e0875fc73ea5260d70585407e4e91d49364ebea7
apple <apple@jd506-22.local>
2018-05-15 15:43:02 +0800

    5.15

---
+++ app/src/main/java/cn/edu/gdmec/android/weatherdemo/mvp/View/IWeatherView.java
@@ -0,0 +1,8 @@
+package cn.edu.gdmec.android.weatherdemo.mvp.View;
+
+/**
+ * Created by apple on 18/5/15.
+ */
+
+public interface IWeatherView {
+}
---
+++ app/src/main/java/cn/edu/gdmec/android/weatherdemo/mvp/model/ILoadListener.java
@@ -0,0 +1,8 @@
+package cn.edu.gdmec.android.weatherdemo.mvp.model;
+
+/**
+ * Created by apple on 18/5/15.
+ */
+
+public interface ILoadListener {
+}
